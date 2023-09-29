26270b8b8a3341c48706fab781be39153f97c61b
JP Lehr <JanPatrick.Lehr@amd.com>
2023-09-29 15:35:10 -0500

    Revert "[OpenMP] Introduce the initial support for OpenMP kernel language (#66844)"
    This reverts commit e997dca3333823ffe2ea3aea288299f551532dcd.

--- clang/lib/AST/StmtProfile.cpp
+++ clang/lib/AST/StmtProfile.cpp
@@ -995,7 +995,6 @@ void OMPClauseProfiler::VisitOMPDoacrossClause(const OMPDoacrossClause *C) {
 }
 void OMPClauseProfiler::VisitOMPXAttributeClause(const OMPXAttributeClause *C) {
 }
-void OMPClauseProfiler::VisitOMPXBareClause(const OMPXBareClause *C) {}
 } // namespace
 
 void
